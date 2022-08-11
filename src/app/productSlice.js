19d36e4ab16227ab34895327fea8c9d8014c5020
Jacob Lopez <lopezjacob2003.jl@gmail.com>
2022-08-10 22:02:19 -0600

    Set up ProductSlice(Cart)

---
+++ src/app/productSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  products: [],
+};
+
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
+
+  reducers: {
+    addToCart: (state, action) => {
+      state.products.push(action.payload)
+    },
+    removeFromCart: (state, action) => {
+      state.products.filter(item === action.payload)
+    },
+    getCartItems: (state) => {
+      console.log(state.products)
+      return state.products
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, getCartItems } = productSlice.actions;
+
+export default productSlice.reducer;
